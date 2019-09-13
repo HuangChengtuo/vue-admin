08a86b77978c2f79d30b1eb6a5d2b1c31d704c58
HuangChengtuo <hct564117373@gmail.com>
2019-09-13 12:42:54 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,22 @@ module.exports = {
     node: true
   },
   'extends': [
-    'plugin:vue/essential',
+    // 'plugin:vue/essential',
+    'plugin:vue/recommended',
     'eslint:recommended'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "vue/max-attributes-per-line": [2,
+      {
+        "singleline": 10,
+        "multiline": {"max": 1, "allowFirstLine": false}
+      }
+    ],
+    'no-var': 2,
+    'prefer-const':2,
+    eqeqeq:[2,'smart']
   },
   parserOptions: {
     parser: 'babel-eslint'
