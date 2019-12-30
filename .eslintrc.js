9e2cc53b0a9341e6744272f60c4a5a07e01c6c47
HuangChengtuo <huangchengtuo@ccclubs.com>
2019-12-30 16:15:25 +0800

    eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,15 +11,25 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    "vue/max-attributes-per-line": [2,
-      {
-        "singleline": 10,
-        "multiline": {"max": 1, "allowFirstLine": false}
+    "vue/html-self-closing": 2,
+    "vue/max-attributes-per-line": [2, {
+      "singleline": 5,
+      "multiline": {
+        "max": 1,
+        "allowFirstLine": false
       }
-    ],
+    }],
     'no-var': 2,
-    'prefer-const':2,
-    eqeqeq:[2,'smart']
+    'prefer-const': 2,
+    eqeqeq: [2, 'smart'],
+    'indent': [2, 2, {'SwitchCase': 1}],
+    'quotes': ['error', 'single'],
+    'space-before-function-paren': [2, 'never'],
+    'no-undef': 0,
+    'vue/html-closing-bracket-newline': [2, {
+      'singleline': 'never',
+      'multiline': 'always'
+    }]
   },
   parserOptions: {
     parser: 'babel-eslint'
